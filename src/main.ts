ce8211cb2c2459bdcd2de95890e4b0274a4804de
d.novak <d.novak@eisgroup.com>
2023-04-10 22:16:13 +0200

    Task 8.1: Deployed to env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { configure as serverlessExpress } from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 
-import * as helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 4000;
+
+let cachedServer: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,10 +15,20 @@ async function bootstrap() {
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
-  app.use(helmet());
+  await app.init();
 
-  await app.listen(port);
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
 bootstrap().then(() => {
   console.log('App is running on %s port', port);
 });
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  cachedServer = cachedServer ?? (await bootstrap());
+  return cachedServer(event, context, callback);
+}
